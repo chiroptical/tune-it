0b037a46cbcdab0e688ab1193c538989926d48e4
Barry <moore0557@gmail.com>
2014-08-12 16:38:38 -0400

    Tune.py now contains all class information

--- tune-it.py
+++ tune-it.py
@@ -18,7 +18,7 @@ Options:
 from docopt import docopt
 from sys import exit
 from os.path import isfile, splitext
-from tune_input import TuneInput
+from Tune import Tune
 
 
 # Begin our script
@@ -26,11 +26,17 @@ try:
     # docopt parses command line options via doc string above
     arguments = docopt(__doc__, version='tune-it.py version 0.0.1')
     
-    # Read input file and process jobs
-    tune = TuneInput(arguments['--input'])
+    # Read input file
+    if not isfile(arguments['--input']) or splitext(arguments['--input'])[-1] not in ['.tune-nw', '.tune-g09']:
+        raise Exception(('Input Error: {} doesn\'t exist or doesn\'t have the ' +
+                         'expected extension, try -h/--help').format(arguments['--input']))
+    tune = Tune(arguments['--input'])
+
+    # What step do we need to complete
+
 
 # Exceptions we may want to handle
-except (KeyboardInterrupt,SystemExit):
+except KeyboardInterrupt:
     print('Interrupt Detected! exiting...')
 
 except Exception as e:
